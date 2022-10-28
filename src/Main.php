c8acd8356cc0c00a7593ca88ff39804c8412fdc3
PullDK <94751329+PullDK@users.noreply.github.com>
2022-10-28 16:40:36 -0300

    Add files via upload

--- src/Main.php
+++ src/Main.php
@@ -1,38 +1,44 @@
 <?php
 
-namespace Corentin503;
+declare(strict_types=1);
 
+namespace PullDK\KillMoney;
+
+use pocketmine\plugin\PluginBase;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\player\PlayerDeathEvent;
 use pocketmine\player\Player;
-use pocketmine\plugin\PluginBase;
 use pocketmine\Server;
+use pocketmine\event\Listener;
 
-class Main extends PluginBase
-{
-    public function onEnable(): void
-    {
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->saveResource();
-    }
+class Main extends PluginBase{
 
-    public static function addMoney(Player $player, int $amount)
-    {
-        $facapi = Server::getInstance()->getPluginManager()->getPlugin("BedrockEconomy")->getDataFolder() . "cooldogedev/BedrockEconomy/api/version" . strtolower("LegacyBEAPI") . ".php";
-        return $facapi->addToPlayerBalance($player->getName(), $amount);
-    }
 
-    public function onAttack(PlayerDeathEvent $event)
-    {
-        $damageCause = $event->getEntity()->getLastDamageCause();
+        public function onEnable(): void
+        {
+            $this->getServer()->getPluginManager()->registerEvents($this, $this);
+            $this->saveResource();
+        }
+    
+        public static function addMoney(Player $player, int $amount)
+        {
+            $facapi = Server::getInstance()->getPluginManager()->getPlugin("BedrockEconomy")->getDataFolder() . "cooldogedev/BedrockEconomy/api/version" . strtolower("LegacyBEAPI") . ".php";
+            return $facapi->addToPlayerBalance($player->getName(), $amount);
+        }
+    
+        public function onAttack(PlayerDeathEvent $event)
+        {
+            $damageCause = $event->getEntity()->getLastDamageCause();
+    
+            if (!$damageCause instanceof EntityDamageByEntityEvent) return;
+            /** @var EntityDamageByEntityEvent $damageCause */
+    
+            if (!$damageCause->getDamager() instanceof Player) return;
+    
+            /** @var Player $damager */
+            $damager = $damageCause->getDamager();
+            self::addMoney($damager, $this->getConfig()->get("moneyadd"));
+        }
+    
 
-        if (!$damageCause instanceof EntityDamageByEntityEvent) return;
-        /** @var EntityDamageByEntityEvent $damageCause */
-
-        if (!$damageCause->getDamager() instanceof Player) return;
-
-        /** @var Player $damager */
-        $damager = $damageCause->getDamager();
-        self::addMoney($damager, $this->getConfig()->get("moneyadd"));
-    }
 }
